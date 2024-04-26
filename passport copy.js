c61143670e20cde255105383b45169527e5eab1c
arkoseal <arkodseal@gmail.com>
2024-04-26 11:15:36 +0530

    passport changes

---
+++ passport copy.js
@@ -0,0 +1,23 @@
+'use strict';
+
+require('./mongoose')();
+var passport = require('passport');
+var User = require('mongoose').model('User');
+var FacebookTokenStrategy = require('passport-facebook-token');
+
+var config = require('./config');
+
+module.exports = function () {
+
+
+    passport.use(new FacebookTokenStrategy({
+            clientID: config.facebookAuth.clientID,
+            clientSecret: config.facebookAuth.clientSecret
+        },
+        function (accessToken, refreshToken, profile, done) {
+            User.upsertFbUser(accessToken, refreshToken, profile, function(err, user) {
+                return done(err, user);
+            });
+        }));
+
+};
